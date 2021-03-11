ad46b464ac10d2d3c889d0d122b3083f1f194707
xryuseix <ryusei143.shootingstar@gmail.com>
2021-03-12 01:36:04 +0900

    docs: add caution message

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,15 +15,29 @@ const SiteIndex = ({ location }) => {
       <SEO title="index" description="SecHack365-Fans Toppage" />
       <Meta title="Home" />
       <h1>SecHack365 非公式 ファンページ</h1>
+      <h5 class="box">
+        <div>
+          <span class="box-title">注意</span>
+          このページは<span style={{ color: 'red', 'font-size': '2em' }}>非公式</span>のファンページです．
+          <br />
+          公式のページはこちらになります．
+        </div>
+        <ul class="officialLink">
+          <li class="officialBtn">
+            <Link href="https://sechack365.nict.go.jp/" style={{ color: '#353A40' }}>
+              Webサイト
+            </Link>
+          </li>
+          <li class="officialBtn">
+            <Link href="https://twitter.com/SecHack365" style={{ color: '#353A40' }}>
+              Twitter
+            </Link>
+          </li>
+        </ul>
+      </h5>
       <div class="icon">
         <StaticImage src="../images/top.png" alt="icon" style={{ border: '50' }} />
       </div>
-      <br />
-      <h2 style={{ 'text-align': 'center' }}>
-        <Link href="https://sechack365.nict.go.jp/" target="_blank" rel="noopener noreferrer">
-          SecHack365 公式ページはこちら
-        </Link>
-      </h2>
     </Layout>
   )
 }
