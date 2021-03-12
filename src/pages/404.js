0028caa821a3e93b2760c40b50ef2a7cc5d7f46b
xryuseix <ryusei143.shootingstar@gmail.com>
2021-03-12 15:08:51 +0900

    feat: 404ページ追加

---
+++ src/pages/404.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { StaticImage } from 'gatsby-plugin-image'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import Meta from '../components/meta'
+
+const SiteIndex = ({ location }) => {
+  return (
+    <Layout location={location}>
+      <SEO title="404" description="404page" />
+      <Meta title="404" />
+      <h1>404 Not Found</h1>
+      <p>あれ，もしかしてページをなくし物されていますか？そんな時はこちら！！</p>
+      <h3>
+        「Tsukushi Speaker 僕だけが失くしモノをしない〜あれ、君たちまだインターネットにカメラ繋いで
+        ハッカーに監視されてるの？僕はオフラインで安全です〜」
+      </h3>
+      <Link
+        href="https://sechack365.nict.go.jp/achievement/2020/pdf/2020_01.pdf"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{ 'text-align': 'center' }}
+      >
+        <StaticImage src="../images/tsukushi_speaker.png" alt="tsuku4" />
+      </Link>
+    </Layout>
+  )
+}
+
+export default SiteIndex
