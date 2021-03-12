401e4cd4218df24335aeaa67619f24ba523438d5
xryuseix <ryusei143.shootingstar@gmail.com>
2021-03-12 14:51:35 +0900

    style: ．to 。

--- src/pages/2020/blog.js
+++ src/pages/2020/blog.js
@@ -11,10 +11,10 @@ const SiteIndex = ({ location }) => {
   const siteTitle = 'sh365 blog'
   return (
     <Layout location={location} title={siteTitle}>
-      <SEO title="All posts" description="2020年度にSecHack365に参加したトレーニーによる参加ブログのまとめです．" />
+      <SEO title="All posts" description="2020年度にSecHack365に参加したトレーニーによる参加ブログのまとめです。" />
       <Meta title="SH365 Blog" />
       <h1>SecHack365 参加記 まとめ【随時更新】</h1>
-      <p>2020年度にSecHack365に参加したトレーニーによる参加ブログのまとめです．(順不同)</p>
+      <p>2020年度にSecHack365に参加したトレーニーによる参加ブログのまとめです。(順不同)</p>
       <div>
         <h2 class="coursettl c0 c01">表現駆動コース</h2>
         <div>
