07139ded7b35b69f0024827817add7d916a4cf0b
Krouton <me+git@tokinia.me>
2021-03-15 12:21:56 +0900

    JSONからJSXをやるようにした

--- src/pages/2020/blog.js
+++ src/pages/2020/blog.js
@@ -7,6 +7,102 @@ import Meta from '../../components/meta'
 import { StaticImage } from 'gatsby-plugin-image'
 import './blog.css'
 
+// 表現駆動のエントリデータ
+const ExprData = [
+  {
+    author: '中嶋 桃香',
+    title: '「SecHack365修了しました」',
+    blogHRef: 'https://momoka-mero.hatenablog.com/entry/2021/03/06/232920',
+    blogHostedAt: 'hatenablog.com',
+    achivements: [
+      {
+        title: 'Tsukushi Speaker 僕だけが失くしモノをしない',
+        href: 'https://sechack365.nict.go.jp/achievement/2020/pdf/2020_01.pdf'
+      }
+    ]
+  },
+  {
+    author: '酒井 義史',
+    title: '「SecHack365 2020表現駆動コース参加体験記」',
+    blogHRef: 'https://yoshistl.hatenablog.com/entry/2021/03/08/024337',
+    blogHostedAt: 'hatenablog.com',
+    achivements: [
+      {
+        title: 'Tsukushi Speaker 僕だけが失くしモノをしない',
+        href: 'https://sechack365.nict.go.jp/achievement/2020/pdf/2020_01.pdf'
+      }
+    ]
+  },
+  {
+    author: '玉田 遍',
+    title: '「たのしかったSecHack365」',
+    blogHRef: 'https://iiimiiino.hatenablog.com/entry/2021/03/08/232321',
+    blogHostedAt: 'hatenablog.com',
+    achivements: [
+      {
+        title: 'Tsukushi Speaker 僕だけが失くしモノをしない',
+        href: 'https://sechack365.nict.go.jp/achievement/2020/pdf/2020_01.pdf'
+      },
+      {
+        title: '距離を縮めるチャット Approachat',
+        href: 'https://sechack365.nict.go.jp/achievement/2020/pdf/2020_02.pdf'
+      }
+    ]
+  }
+]
+
+// 学習駆動コースのデータ
+// eslint-disable-next-line no-unused-vars
+const StudyData = []
+
+// 開発駆動のエントリデータ
+const DevData = [
+  {
+    author: '寺嶋 友哉',
+    title: '「SecHack365を修了しました」',
+    blogHRef: 'https://terassyi.net/posts/2021/03/09/sechack365.html',
+    blogHostedAt: 'terassyi.net',
+    achivements: [
+      {
+        title: 'セキュリティ演習環境構築支援ツールKAKOI',
+        href: 'https://sechack365.nict.go.jp/achievement/2020/pdf/2020_20.pdf'
+      }
+    ]
+  }
+]
+
+// 思索駆動のエントリデータ
+const ThinkData = [
+  {
+    author: '福地 成彦',
+    title: '「2020年度のSecHack365を完走しました」',
+    blogHRef: 'https://fqc.hatenablog.com/entry/2021/03/14/092046',
+    blogHostedAt: 'hatenablog.com',
+    achivements: [
+      {
+        title: 'FairTorch: 深層学習の公平性ライブラリ',
+        href: 'https://sechack365.nict.go.jp/achievement/2020/pdf/2020_26.pdf'
+      }
+    ]
+  }
+]
+
+// 研究駆動のエントリデータ
+const ResearchData = [
+  {
+    author: '石川 琉聖',
+    title: '「SecHack365 参加記',
+    blogHRef: 'https://xryuseix.hatenablog.com/entry/2021/03/08/173442',
+    blogHostedAt: 'hatenablog.com',
+    achivements: [
+      {
+        title: 'ソースコードの盗作判定システム SA-Plag',
+        href: 'https://sechack365.nict.go.jp/achievement/2020/pdf/2020_28.pdf'
+      }
+    ]
+  }
+]
+
 /**
  * TableRow でのみの使用を想定している
  * @param {string} title - 作品のタイトル
@@ -72,51 +168,15 @@ const SiteIndex = ({ location }) => {
               <th class="sh365_blog">参加記</th>
               <th class="sh365_hack">成果物</th>
             </tr>
-            <TableRow
-              author={'中嶋 桃香'}
-              title={'「SecHack365修了しました」'}
-              blogHRef={'https://momoka-mero.hatenablog.com/entry/2021/03/06/232920'}
-              blogHostedAt={'hatenablog.com'}
-              achivements={[
-                {
-                  title: 'Tsukushi Speaker 僕だけが失くしモノをしない',
-                  href: 'https://sechack365.nict.go.jp/achievement/2020/pdf/2020_01.pdf'
-                }
-              ]}
-            />
-            <TableRow
-              author={'酒井 義史'}
-              title={'「SecHack365 2020表現駆動コース参加体験記」'}
-              blogHRef={'https://yoshistl.hatenablog.com/entry/2021/03/08/024337'}
-              blogHostedAt={'hatenablog.com'}
-              achivements={[
-                {
-                  title: 'Tsukushi Speaker 僕だけが失くしモノをしない',
-                  href: 'https://sechack365.nict.go.jp/achievement/2020/pdf/2020_01.pdf'
-                }
-              ]}
-            />
-            <TableRow
-              author={'玉田 遍'}
-              title={'「たのしかったSecHack365」'}
-              blogHRef={'https://iiimiiino.hatenablog.com/entry/2021/03/08/232321'}
-              blogHostedAt={'hatenablog.com'}
-              achivements={[
-                {
-                  title: 'Tsukushi Speaker 僕だけが失くしモノをしない',
-                  href: 'https://sechack365.nict.go.jp/achievement/2020/pdf/2020_01.pdf'
-                },
-                {
-                  title: '距離を縮めるチャット Approachat',
-                  href: 'https://sechack365.nict.go.jp/achievement/2020/pdf/2020_02.pdf'
-                }
-              ]}
-            />
+            {ExprData.map((data) => (
+              <TableRow {...data} />
+            ))}
           </table>
         </div>
       </div>
       <div>
         <h2 class="coursettl c0 c02">学習駆動コース</h2>
+        {/* エントリが出たら突っ込む */}
       </div>
       <div>
         <h2 class="coursettl c0 c03">開発駆動コース</h2>
@@ -128,18 +188,9 @@ const SiteIndex = ({ location }) => {
               <th class="sh365_blog">参加記</th>
               <th class="sh365_hack">成果物</th>
             </tr>
-            <TableRow
-              author={'寺嶋 友哉'}
-              title={'「SecHack365を修了しました」'}
-              blogHRef={'https://terassyi.net/posts/2021/03/09/sechack365.html'}
-              blogHostedAt={'terassyi.net'}
-              achivements={[
-                {
-                  title: 'セキュリティ演習環境構築支援ツールKAKOI',
-                  href: 'https://sechack365.nict.go.jp/achievement/2020/pdf/2020_20.pdf'
-                }
-              ]}
-            />
+            {DevData.map((data) => (
+              <TableRow {...data} />
+            ))}
           </table>
         </div>
       </div>
@@ -153,18 +204,9 @@ const SiteIndex = ({ location }) => {
               <th class="sh365_blog">参加記</th>
               <th class="sh365_hack">成果物</th>
             </tr>
-            <TableRow
-              author={'福地 成彦'}
-              title={'「2020年度のSecHack365を完走しました」'}
-              blogHRef={'https://fqc.hatenablog.com/entry/2021/03/14/092046'}
-              blogHostedAt={'hatenablog.com'}
-              achivements={[
-                {
-                  title: 'FairTorch: 深層学習の公平性ライブラリ',
-                  href: 'https://sechack365.nict.go.jp/achievement/2020/pdf/2020_26.pdf'
-                }
-              ]}
-            />
+            {ThinkData.map((data) => (
+              <TableRow {...data} />
+            ))}
           </table>
         </div>
       </div>
@@ -178,18 +220,9 @@ const SiteIndex = ({ location }) => {
               <th class="sh365_blog">参加記</th>
               <th class="sh365_hack">成果物</th>
             </tr>
-            <TableRow
-              author={'石川 琉聖'}
-              title={'「SecHack365 参加記'}
-              blogHRef={'https://xryuseix.hatenablog.com/entry/2021/03/08/173442'}
-              blogHostedAt={'hatenablog.com'}
-              achivements={[
-                {
-                  title: 'ソースコードの盗作判定システム SA-Plag',
-                  href: 'https://sechack365.nict.go.jp/achievement/2020/pdf/2020_28.pdf'
-                }
-              ]}
-            />
+            {ResearchData.map((data) => (
+              <TableRow {...data} />
+            ))}
           </table>
         </div>
       </div>
