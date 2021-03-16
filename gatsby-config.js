780e0e89d051d73ea3e7becfe45086e744d2328c
xryuseix <ryusei143.shootingstar@gmail.com>
2021-03-16 15:44:24 +0900

    refactor: 記事はどこか別のリンク先におくので，filesystem要らなかった

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,13 +11,6 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/content/news`,
-        name: `news`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
         name: `images`,
         path: `${__dirname}/src/images/`,
       },
