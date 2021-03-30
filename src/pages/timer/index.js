41e13c3e740932910da64bc5a11163113cf2bcc9
xryuseix <ryusei143.shootingstar@gmail.com>
2021-03-30 21:23:02 +0900

    style: 背景を一部黒に，タイトルの挿入

--- src/pages/timer/index.js
+++ src/pages/timer/index.js
@@ -22,17 +22,22 @@ const SiteIndex = ({ location }) => {
     <Layout location={location}>
       <SEO title="timer" description="†締切駆動コース†" />
       <Meta title="†締切駆動コース†" />
-      {/* 現在時刻の表示エリア */}
-      <p id="RealtimeClockArea" align="center">
-        <b>
-          <font size="+2" color="#ff0000">
-            Wait...
-          </font>
-        </b>
-      </p>
-      {/* カウントダウンタイマーの表示エリア */}
-      <div align="center">
-        <div class="counter" id="counter"></div>
+      <div style={{ 'background-color': 'black' }}>
+        <h3>
+          <b style={{ color: 'white' }}>†限界タイマー†</b>
+        </h3>
+        {/* 現在時刻の表示エリア */}
+        <p id="RealtimeClockArea" align="center">
+          <b>
+            <font size="+2" color="#ff0000">
+              Wait...
+            </font>
+          </b>
+        </p>
+        {/* カウントダウンタイマーの表示エリア */}
+        <div align="center">
+          <div class="counter" id="counter"></div>
+        </div>
       </div>
     </Layout>
   )
