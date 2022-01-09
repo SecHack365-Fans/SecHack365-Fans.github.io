cb4bab9a3862ae821ab0b13d3355ce75a31a8409
xryuseix <ryusei143.shootingstar@gmail.com>
2022-01-09 12:59:56 +0900

    feat: Footerを作成

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -1,9 +1,14 @@
 import { VFC } from "react";
+import styles from "./Footer.module.css";
 
 const Footer: VFC = () => {
   const date = new Date();
   const year = date.getFullYear();
-  return <div>© 2021-{year} SecHack365-Fans All Right Reserved.</div>;
+  return (
+    <div className={styles.footer}>
+      © 2021-{year} SecHack365-Fans All Right Reserved.
+    </div>
+  );
 };
 
 export default Footer;
