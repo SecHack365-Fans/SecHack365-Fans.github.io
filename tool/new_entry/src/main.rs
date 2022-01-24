acd68ada830e5bcd04d5a81a76efd5a4e6f7bde5
xryuseix <ryusei143.shootingstar@gmail.com>
2022-01-24 21:06:15 +0900

    fix: new_entryツールの出力項目に現状不要な項目のdateとcourseを削除

--- tool/new_entry/src/main.rs
+++ tool/new_entry/src/main.rs
@@ -1,52 +1,6 @@
-use serde::{Deserialize, Serialize, Serializer};
-use std::{
-    io::{self, stdout, Write},
-    time::UNIX_EPOCH,
-};
+use serde::Serialize;
+use std::io::{self, stdout, Write};
 use url::Url;
-
-#[derive(Debug)]
-enum Course {
-    // 表現
-    Expr,
-    // 学習
-    Learn,
-    // 開発
-    Dev,
-    // 思索
-    Think,
-    // 研究
-    Research,
-}
-
-impl Serialize for Course {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        let s = match self {
-            Course::Expr => "expr",
-            Course::Learn => "learn",
-            Course::Dev => "dev",
-            Course::Think => "think",
-            Course::Research => "research",
-        };
-        serializer.serialize_str(s)
-    }
-}
-
-impl Course {
-    fn from_str(s: &str) -> Option<Self> {
-        match s {
-            "1" => Some(Course::Expr),
-            "2" => Some(Course::Learn),
-            "3" => Some(Course::Dev),
-            "4" => Some(Course::Think),
-            "5" => Some(Course::Research),
-            _ => None,
-        }
-    }
-}
 
 #[derive(Debug, Serialize)]
 struct Achivement {
@@ -56,7 +10,6 @@ struct Achivement {
 
 #[derive(Debug, Serialize)]
 struct Entry {
-    course: Course,
     author: String,
     title: String,
     #[serde(rename = "blogHRef")]
@@ -64,9 +17,6 @@ struct Entry {
     #[serde(rename = "blogHostedAt")]
     blog_hosted_at: String,
     achivements: Vec<Achivement>,
-    // 日付でソートする？
-    #[serde(rename = "inputDate")]
-    input_date: u64,
 }
 
 fn get_input_with_question(question: &str) -> io::Result<String> {
@@ -82,22 +32,6 @@ fn get_input_with_question(question: &str) -> io::Result<String> {
 }
 
 fn main() -> io::Result<()> {
-    let course = loop {
-        let input_course = get_input_with_question(
-            "あなたの所属していたコースは？
-表現 -> 1
-学習 -> 2
-開発 -> 3
-思索 -> 4
-研究 -> 5
-> ",
-        )?;
-        if let Some(course) = Course::from_str(&input_course) {
-            break course;
-        } else {
-            println!("想定外の入力がされました、もう1回！");
-        }
-    };
     let author = get_input_with_question("あなたの名前は？")?;
     let title = get_input_with_question("参加記のタイトルは？")?;
     let blog_href = get_input_with_question("ブログのURLは？")?;
@@ -119,17 +53,11 @@ fn main() -> io::Result<()> {
         }
     }
     let entry = Entry {
-        course,
         author,
         title,
         blog_href,
         blog_hosted_at,
         achivements,
-        input_date: std::time::SystemTime::now()
-            .duration_since(UNIX_EPOCH)
-            .unwrap()
-            .as_secs()
-            .into(),
     };
     let j = serde_json::to_string(&entry)?;
     println!("{}", j);
