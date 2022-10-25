3af1f967e668400ecba875db83e68cdb59b007a5
xryuseix <ryusei143.shootingstar@gmail.com>
2022-10-25 19:02:46 +0900

    feat: 2022用のページを作成

--- pages/ctf/TsukuCTF2022/index.tsx
+++ pages/ctf/TsukuCTF2022/index.tsx
@@ -63,15 +63,37 @@ const TsukuCTF = ({
   return (
     <Layout title="TsukuCTF2022 Writeups" description="TsukuCTF2022 Writeups">
       <h1>TsukuCTF 2022</h1>
+
       <div className={styles.ctfDetails}>
         <a
           href="https://tsukuctf.sechack365.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <img src="../../../images/tsukuctf.png" alt="TsukuCTF" />
-          <div className={styles.ctfLink}>https://tsukuctf.sechack365.com/</div>
+          <img src="../../../images/tsukuctf-logo.png" alt="TsukuCTF" />
+          <div className={styles.ctfLink}>Registration for TsukuCTF</div>
         </a>
+        <div className={styles.ctfLink}>
+          2022/10/22 12:20pm GMT+9 ~ 2022/10/23 18:00pm GMT+9 (29h40m)
+        </div>
+        <div className={styles.ctfLink}>
+          Follow us on{" "}
+          <a
+            href="https://twitter.com/tsukuctf"
+            target="_blank"
+            rel="noopener noreferer noreferrer"
+          >
+            Twitter(@tsukuctf)
+          </a>{" "}
+          or{" "}
+          <a
+            href="https://discord.gg/EEsfKWm9"
+            target="_blank"
+            rel="noopener noreferer noreferrer"
+          >
+            Discord
+          </a>
+        </div>
       </div>
       <hr />
       <div className={styles.writeups}>
