bd9ff51a4cf9d5ca6c9fb5b026974944f9088a13
xryuseix <ryusei143.shootingstar@gmail.com>
2023-12-17 03:06:01 +0900

    feat: 404 Styles

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,13 +1,14 @@
 import { Layout } from "../components/Layout";
 
-import styles from "../styles/404.module.css";
+import { s } from "../styles/404.stylex";
+import stylex from "@stylexjs/stylex";
 
 const Custom404 = () => {
   return (
     <Layout title="404 not found">
       <h1>404 Not Found</h1>
-      <div className={styles.menu}>
-        <img src="/images/tsuku4.png" alt="tsuku4" />
+      <div {...stylex.props(s.container)}>
+        <img src="/images/tsuku4.png" alt="tsuku4" {...stylex.props(s.logo)} />
       </div>
     </Layout>
   );
---
+++ styles/404.stylex.ts
@@ -0,0 +1,12 @@
+import stylex from "@stylexjs/stylex";
+
+export const s = stylex.create({
+  container: {
+    position: "fixed",
+    bottom: 0,
+    textAlign: "center",
+  },
+  logo: {
+    width: "90%",
+  },
+});
