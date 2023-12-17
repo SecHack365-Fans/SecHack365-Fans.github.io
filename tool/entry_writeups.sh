b3bad1b20f2c1f4f085bd49f1b0d0a3f77ad71a0
xryuseix <ryusei143.shootingstar@gmail.com>
2023-12-17 20:55:46 +0900

    fix: entry_writeups.sh

--- tool/entry_writeups.sh
+++ tool/entry_writeups.sh
@@ -9,8 +9,12 @@ for category in "${categories[@]}"; do
     if [ -d ../TsukuCTF$YEAR/$category ]; then
         for filename in ../TsukuCTF2023/$category/*; do
             base=$(basename $filename)
+            filepath=$filename/writeup/README.md
+            if [ ! -f $filepath ]; then
+                continue
+            fi
             mkdir -p pages/ctf/TsukuCTF2023/writeups/$base
-            cp -f $filename/README.md pages/ctf/TsukuCTF2023/writeups/$base/README.md
+            cp -f $filename/writeup/README.md pages/ctf/TsukuCTF2023/writeups/$base/README.md
         done
     fi
 done
