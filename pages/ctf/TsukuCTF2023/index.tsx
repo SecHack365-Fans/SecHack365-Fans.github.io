20c49c5164f929896276c696ddc4b6493091443b
xryuseix <ryusei143.shootingstar@gmail.com>
2023-12-20 07:55:47 +0900

    fix: add date

--- pages/ctf/TsukuCTF2023/index.tsx
+++ pages/ctf/TsukuCTF2023/index.tsx
@@ -4,7 +4,7 @@ import { TsukuCTFWriteup } from "../../../components/TsukuCTF";
 import { AllPostedData } from "../../../components/writeup";
 
 const ctfId = "TsukuCTF2023";
-const date = "";
+const date = "2023/12/09 12:20pm ~ 2023/12/10 18:00pm";
 
 const TsukuCTF = ({ allPostsData }: AllPostedData) => {
   return TsukuCTFWriteup({ allPostsData, ctfId, date });
